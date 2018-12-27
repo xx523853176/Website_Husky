b3029e427de9626379dee34eb75c8bb2e9986032
Husky <523853176@qq.com>
2018-12-27 23:44:04 +0800

    create-react-app original

---
+++ build/precache-manifest.6f63d4e921dcb2c7c6282772d5979dec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d60200cd42564ccdd40",
+    "url": "/static/js/main.9d60200c.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9d60200cd42564ccdd40",
+    "url": "/static/css/main.59498f39.chunk.css"
+  },
+  {
+    "revision": "fa7edc7400d2f01dbe9e4a897e6c3a36",
+    "url": "/index.html"
+  }
+];
